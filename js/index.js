dc96a8c2530c5b047d15b0b1eb34a29aaad6aff7
AsadullahBadhan <asadullah.badhan@gmail.com>
2022-09-26 19:56:51 +0600

    carousel added

--- js/index.js
+++ js/index.js
@@ -7,3 +7,29 @@ hamburger.addEventListener('click', function (e) {
   navItems.classList.toggle('hidden');
   console.log(navItems)
 })
+
+let slideIndex = 1;
+showSlides(slideIndex)
+
+//dots functionality
+function currentSlide(index) {
+  showSlides(slideIndex = index);
+}
+
+function showSlides(index) {
+  const reviews = document.querySelectorAll('.review');
+  const dots = document.querySelectorAll('.dot');
+  if (index > reviews.length) { slideIndex = 1 }
+  for (let i = 0; i < reviews.length; i++) {
+    reviews[i].style.display = 'none';
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace('active', '');
+  }
+  reviews[slideIndex - 1].style.display = 'flex';
+  dots[slideIndex - 1].className += ' active';
+}
+
+setInterval(() => {
+  showSlides(slideIndex += 1)
+}, 3000)
